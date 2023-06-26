638a742a8eff8e76e4a47710dfcffc1db1bb395e
wynterjones <wynter@monetizedesign.com>
2023-06-25 22:42:23 -0400

    as

--- elements/image.js
+++ elements/image.js
@@ -3,7 +3,7 @@ const image = data => {
   const output = blueprint('Image/V1', data.id, data.parentId, data.index, element)
   const css = properties.css(element.id, 'image')
   const borderRadius = properties.borderRadius(css)
-  const theParams = params(css, 'element', element.id, 'image')
+  const theParams = params(css, 'element', element.id)
 
   theParams['default-aspect-ratio'] = '1280 / 853'
   theParams['--style-padding-horizontal'] = 0
