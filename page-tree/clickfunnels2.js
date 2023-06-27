16496a622fb19e7b63a0f5c6d1d22b6496fa18a3
wynterjones <wynter@monetizedesign.com>
2023-06-27 14:26:30 -0400

    .

--- page-tree/clickfunnels2.js
+++ page-tree/clickfunnels2.js
@@ -64,7 +64,7 @@ const clickfunnels2_pagetree = (clickfunnels_classic_page_tree, new_page_tree) =
       }
 
       if (parseInt(checkCSS[0]) === 0) {
-        data.vertical = 'bottom'
+        data.vertical = 'auto'
       }
     }
 
@@ -78,7 +78,7 @@ const clickfunnels2_pagetree = (clickfunnels_classic_page_tree, new_page_tree) =
       }
 
       if (parseInt(checkCSS[1]) === 0) {
-        data.horizontal = 'right'
+        data.horizontal = 'auto'
       }
     }
 
